048760a30f41e9f7becef0ee6342022b899f0a48
Ken Cheng <kencheng@github.com>
2026-06-04 22:04:00 +0800

    fix: use base '/dist/' for GitHub Pages legacy build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Student-mission-app/',
+  base: '/dist/',
   plugins: [vue()],
   server: {
     host: true,
